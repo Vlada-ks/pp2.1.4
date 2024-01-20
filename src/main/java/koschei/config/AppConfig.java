7061a2069253c7825f88f0235b0e6dd53ee052e8
Vlada-ks <122997495+Vlada-ks@users.noreply.github.com>
2024-01-20 18:57:03 +0400

    2.1.4

---
+++ src/main/java/koschei/config/AppConfig.java
@@ -0,0 +1,26 @@
+package koschei.config;
+
+import koschei.models.Egg6;
+import koschei.models.Island2;
+import koschei.models.Needle7;
+import koschei.models.Ocean1;
+import koschei.models.Wood3;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "koschei")
+public class AppConfig {
+
+    @Bean
+    public static Island2 getIsland(Wood3 wood) {
+        return new Island2(wood);
+    }
+//    @Bean
+//    public static Egg6 getEgg6(Needle7 needle) {
+//        return Egg6(needle);
+//
+//    }
+}
